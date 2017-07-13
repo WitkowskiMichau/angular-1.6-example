b584ef46c643b971d50cf22f93ee01475a9fbe46
Michal Witkowski <batoponton@gmail.com>
2017-07-13 14:30:57 +0200

    Refactorings

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,7 +1,7 @@
 import angular from 'angular';
-import { AppComponent, AppController } from './app.component';
-import { ComponentsModule } from './components/components.module';
-import { CommonModule } from './common/common.module';
+import {AppComponent, AppController} from './app.component';
+import {ComponentsModule} from './components/components.module';
+import {CommonModule} from './common/common.module';
 
 import '../style/app.css';
 
